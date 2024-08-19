bbb32ed10f5083d08d7c9022084b51f980d36f3b
sudharsansayeeramgithub <130204265+sudharsansayeeramgithub@users.noreply.github.com>
2024-08-19 16:18:37 +0530

    Time: 0 ms (100%), Space: 40.5 MB (45.92%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minSteps(int n) {
+        if(n==1) return 0;
+        int steps=0,factor=2;
+        while(n>1){
+            while(n%factor==0){
+                steps+=factor;
+                n/=factor;
+            }
+            factor++;
+        }
+        return steps;
+    }
+}
