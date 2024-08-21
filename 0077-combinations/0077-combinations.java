e23263364ddd1b6d857c0204352040cbf9217cbb
sudharsansayeeramgithub <130204265+sudharsansayeeramgithub@users.noreply.github.com>
2024-08-21 22:14:02 +0530

    Time: 19 ms (49.17%), Space: 94.7 MB (19.64%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,19 +1,17 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> answer=new ArrayList<>();
-        int[] nums=new int[n];
-        for(int i=0;i<n;i++) nums[i]=i+1;
-        findCombinations(0,k,nums,answer,new ArrayList<>());
+        findCombinations(1,k,n,answer,new ArrayList<>());
         return answer;
     }
-    public void findCombinations(int index,int k,int[] nums,List<List<Integer>> answer,List<Integer> part){
+    public void findCombinations(int index,int k,int n,List<List<Integer>> answer,List<Integer> part){
         if(part.size()==k){
             answer.add(new ArrayList<>(part));
             return;
         }
-        for(int i=index;i<nums.length;i++){
-            part.add(nums[i]);
-            findCombinations(i+1,k,nums,answer,part);
+        for(int i=index;i<=n;i++){
+            part.add(i);
+            findCombinations(i+1,k,n,answer,part);
             part.remove(part.size()-1);
         }
     }
