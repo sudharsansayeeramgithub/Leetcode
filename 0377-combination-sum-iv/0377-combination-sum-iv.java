da220734f303e1b0d63a92c351c1805d0ad5a09c
sudharsansayeeramgithub <130204265+sudharsansayeeramgithub@users.noreply.github.com>
2024-08-21 16:38:25 +0530

    Time: 1 ms (72.34%), Space: 40.6 MB (76.63%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int sumCount[] = new int[target + 1];
+        sumCount[0] = 1;
+        int sz = nums.length;
+        for(int sum = 1; sum <= target; sum++){
+            for(int indx = 0; indx < sz; indx++){
+                if(nums[indx] <= sum){
+                    sumCount[sum] = (sumCount[sum] + sumCount[sum-nums[indx]]);
+                }
+            }
+        }
+        return sumCount[target];
+    }
+}
